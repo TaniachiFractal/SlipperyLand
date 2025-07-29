1ceff13bc697aab81e34f6a64a41cc270fb68fe8
TaniachiFractal <tnmslv@yandex.ru>
2025-07-29 18:54:11 +0300

    Map renderer works

--- SlipperyLand/Converters/BitmapConverter.cs
+++ SlipperyLand/Converters/BitmapConverter.cs
@@ -1,47 +1,40 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Globalization;
-using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
 namespace SlipperyLand.Converters
 {
     /// <summary>
-    /// <see cref="Bitmap"/> and <see cref="BitmapImage"/> converter
+    /// <see cref="Bitmap"/> and <see cref="BitmapSource"/> converter
     /// </summary>
     public class BitmapConverter : IValueConverter
     {
         /// <summary>
-        /// <see cref="Bitmap"/> to <see cref="BitmapImage"/>
+        /// <see cref="Bitmap"/> to <see cref="BitmapSource"/>
         /// </summary>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null)
-            { return null; }
-
-            var bitmap = (Bitmap)value;
-            using var memory = new MemoryStream();
-            bitmap.Save(memory, ImageFormat.Png);
-            memory.Position = 0;
-
-            var bitmapImage = new BitmapImage();
-            bitmapImage.BeginInit();
-            bitmapImage.StreamSource = memory;
-            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-            bitmapImage.EndInit();
-            bitmapImage.Freeze();
-
-            return bitmapImage;
+            if (value is Bitmap bmp && bmp != null)
+            {
+                var bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    bmp.GetHbitmap(),
+                    IntPtr.Zero,
+                    System.Windows.Int32Rect.Empty,
+                    BitmapSizeOptions.FromWidthAndHeight(bmp.Width, bmp.Height));
+                return bitmapSource;
+            }
+            return null;
         }
 
         /// <summary>
-        /// <see cref="BitmapImage"/> to <see cref="Bitmap"/>
+        /// <see cref="BitmapSource"/> to <see cref="Bitmap"/>
         /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return new Bitmap(((BitmapImage)value).StreamSource);
+            return new Bitmap(0, 0);
         }
     }
 }
