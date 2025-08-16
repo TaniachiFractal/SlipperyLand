0d7b27cc0803177ad1e1615e94ad5b3cc3d7a1e9
TaniachiFractal <tnmslv@yandex.ru>
2025-08-16 23:53:41 +0300

    wip

--- MainGame/SlipperyLand/Properties/AssemblyInfo.cs
+++ MainGame/SlipperyLand/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SlipperyLand")]
+[assembly: AssemblyTitle("Slippery Land")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Taniachi Fractal")]
@@ -15,6 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
